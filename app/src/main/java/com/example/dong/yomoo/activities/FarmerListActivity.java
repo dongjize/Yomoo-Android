dc76913966da9d2ed8303c477947afe6e9ca1f76
dongjize <justecommeca@163.com>
2018-01-03 22:59:44 +0800

    temp

--- app/src/main/java/com/example/dong/yomoo/activities/FarmerListActivity.java
+++ app/src/main/java/com/example/dong/yomoo/activities/FarmerListActivity.java
@@ -1,10 +1,13 @@
 package com.example.dong.yomoo.activities;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.widget.ListView;
 
 import com.example.dong.yomoo.R;
 import com.example.dong.yomoo.entities.users.Farmer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,18 +16,44 @@ import java.util.List;
  * Created by dong on 17/12/2017.
  */
 
-public class FarmerListActivity extends BaseActivity {
+public class FarmerListActivity extends BaseActivity implements SwipeRefreshLayout.OnRefreshListener {
 
     private List<Farmer> farmerList;
+    private FarmerListAdapter mAdapter;
+    private ListView listView;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private String offset = "0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.farmer_info_list_activity);
+
+        initToolbar();
+
+        farmerList = new ArrayList<>();
+        swipeRefreshLayout = findViewById(R.id.swipe_refresh);
+        swipeRefreshLayout.setOnRefreshListener(this);
+        listView = findViewById(R.id.list_view);
+
+        getFarmerList();
     }
 
     @Override
     protected void initToolbar() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+    }
+
+    @Override
+    public void onRefresh() {
+        offset = "0";
+    }
+
+    private void getFarmerList() {
 
     }
 }
--- app/src/main/java/com/example/dong/yomoo/entities/users/Farmer.java
+++ app/src/main/java/com/example/dong/yomoo/entities/users/Farmer.java
@@ -30,6 +30,10 @@ public class Farmer extends User {
         setIntro(user.getIntro());
     }
 
+    public String getAddress() {
+        return getVillage() + " " + getGroup() + " " + getStreetNum();
+    }
+
     public String getPhone() {
         return phone;
     }
