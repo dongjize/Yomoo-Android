9c8d1420139444d9a63329f05c6507522c6938f2
Dong <jason.dong@sap.com>
2018-01-13 23:59:29 +0800

    office

--- app/src/main/java/com/example/dong/yomoo/activities/farmer/services/tab1/LiveStockDemandAdapter.java
+++ app/src/main/java/com/example/dong/yomoo/activities/farmer/services/tab1/LiveStockDemandAdapter.java
@@ -1,10 +1,8 @@
 package com.example.dong.yomoo.activities.farmer.services.tab1;
 
 import android.content.Context;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import com.example.dong.yomoo.R;
@@ -19,30 +17,18 @@ import java.util.List;
 
 public class LiveStockDemandAdapter extends BaseListAdapter<LivestockDemand> {
 
-    private Context context;
-    private List<LivestockDemand> livestockDemandList;
-    private LayoutInflater mInflater;
-
     public LiveStockDemandAdapter(Context context, List<LivestockDemand> list) {
         super(context, list);
     }
 
-//    public LiveStockDemandAdapter(Context context, List<LivestockDemand> livestockDemandList) {
-//        this.context = context;
-//        this.livestockDemandList = livestockDemandList;
-//        mInflater = LayoutInflater.from(context);
-//    }
-
-
-
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder holder;
-        LivestockDemand demand = livestockDemandList.get(position);
+        LivestockDemand demand = mList.get(position);
         if (convertView == null) {
             convertView = mInflater.inflate(R.layout.livestock_demand_list_item, parent, false);
             holder = new ViewHolder();
-            holder.tvTitle = convertView.findViewById(R.id.tv_fodder_title);
+            holder.tvTitle = convertView.findViewById(R.id.tv_title);
             holder.tvPublisher = convertView.findViewById(R.id.tv_publisher_name);
             holder.tvCreatedAt = convertView.findViewById(R.id.tv_created_date);
             convertView.setTag(holder);
